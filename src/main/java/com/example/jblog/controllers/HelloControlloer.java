52c69912240e9c07a1eb780f1b775a9156dfd600
Unknown <algml812@hanmail.net>
2018-07-18 20:09:34 +0900

    project setting

---
+++ src/main/java/com/example/jblog/controllers/HelloControlloer.java
@@ -0,0 +1,12 @@
+package com.example.jblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloControlloer {
+	@RequestMapping("/")
+	public String hello() {
+		return "test/hello"; 
+	}
+}
